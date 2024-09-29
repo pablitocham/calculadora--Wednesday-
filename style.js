ab99a238b317f237f8c278b6956db7405ff9ffe6
Juan Pablo Chamorro <jpablocham@gmail.com>
2024-09-28 22:43:30 -0300

    subiendo

--- style.js
+++ style.js
@@ -1,34 +1,33 @@
-
-
 function agregar(valor) {
     document.getElementById('pantalla').value += valor;
 }
 
-
 function borrar() {
     document.getElementById('pantalla').value = '';
 }
-
-
 
 function calcular() {
     const valorPantalla = document.getElementById('pantalla').value;
     const resultado = eval(valorPantalla);
     document.getElementById('pantalla').value = resultado;
 }
-
-document.addEventListener('keydown', function(event){
+document.addEventListener('keydown', function (event) {
     const key = event.key;
-    const validKeys = ['0','1','2','3','4','5','6','7','8','9','.','+','-','*','/'];
-    if(validKeys.includes(key)){
+    const validKeys = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.', '+', '-', '*', '/'];
+    if (validKeys.includes(key)) {
         agregar(key);
-    }else if (key ==='Enter' ){
+    } else if (key === 'Enter') {
         calcular();
-    }else if (key === 'Backspace'){
+    } else if (key === 'Backspace') {
         borrar();
     }
 });
-function playAudio(){
+function playAudio() {
     let audio = document.getElementById("bloodyMary");
-    audio.play()
+
+    if (audio.paused) {
+        audio.play()
+    } else {
+        audio.pause()
+    }
 }
